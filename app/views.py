3022937b3f20d32a2b640277881b9efb98b0593e
SureshK2004 <sureshksureshk04gmail.com>
2024-12-14 15:00:51 +0530

    ms

--- app/views.py
+++ app/views.py
@@ -5,5 +5,8 @@ from django.shortcuts import redirect
 
 @login_required
 def redirect_to_admin(request):
-    return redirect('/admin/')  # Redirect authenticated users to the admin page
+    return redirect('adminpannel')#Redirect authenticated users to the admin page
 
+def adminpannel(request):
+    return render(request,'app.html')
+  
